a98391ad9ad37110999f69139c457926e9d20091
otegami <otegami@clear-code.com>
2024-03-11 17:37:00 +0900

    Support for child_pages_count macro in WikiContentVersion

--- lib/wiki_extensions_child_pages_count_macro.rb
+++ lib/wiki_extensions_child_pages_count_macro.rb
@@ -5,7 +5,7 @@ module WikiExtensionsChildPagesCountMacro
     desc "Displays the number of child pages.\n\n" +
          "{{child_page_count}}\n"
     macro :child_pages_count do |obj, args|
-      raise t(:error_child_pages_count_macro) unless obj.is_a?(WikiContent)
+      raise t(:error_child_pages_count_macro) if [WikiContent, WikiContentVersion].none? { |wiki| obj.is_a?(wiki) }
 
       child_pages = obj.page.descendants
       child_pages.size.to_s
--- test/unit/wiki_extensions_child_pages_count_test.rb
+++ test/unit/wiki_extensions_child_pages_count_test.rb
@@ -4,11 +4,16 @@ class WikiExtensionsChildPagesCountTest < Redmine::HelperTest
   include ApplicationHelper
   include ERB::Util
 
-  fixtures :projects, :wikis, :wiki_pages, :wiki_contents, :issues
+  fixtures :projects, :wikis, :wiki_pages, :wiki_contents, :wiki_content_versions, :issues
 
-  def test_child_pages_count
+  def test_child_pages_count_in_wiki_content
     wiki_page = WikiPage.find(2)
     assert_equal '<p>3</p>', textilizable('{{child_pages_count}}', object: wiki_page.content)
+  end
+
+  def test_child_pages_count_in_wiki_content_version
+    wiki_page = WikiPage.find(2)
+    assert_equal '<p>3</p>', textilizable('{{child_pages_count}}', object: wiki_page.content.versions.first)
   end
 
   def test_child_pages_count_in_issue_page
