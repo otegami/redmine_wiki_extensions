158930ebc7fe699d95f8d1690053c57161fa88db
otegami <otegami@clear-code.com>
2024-03-11 10:53:37 +0900

    Add a test case when users use child_pages_count macro in other page

--- test/unit/wiki_extensions_child_pages_count_test.rb
+++ test/unit/wiki_extensions_child_pages_count_test.rb
@@ -1,15 +1,22 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
-class WikiExtensionsChildPagesCountTest < ActiveSupport::TestCase
+class WikiExtensionsChildPagesCountTest < Redmine::HelperTest
   include ApplicationHelper
   include ERB::Util
 
-  fixtures :projects, :wikis, :wiki_pages, :wiki_contents
+  fixtures :projects, :wikis, :wiki_pages, :wiki_contents, :issues
 
   def test_child_pages_count
     wiki_page = WikiPage.find(2)
 
     assert_equal 3, wiki_page.descendants.size
     assert_equal '<p>3</p>', textilizable('{{child_pages_count}}', object: wiki_page.content)
+  end
+
+  def test_child_pages_count_in_issue_page
+    issue = Issue.first
+    expected_error_macro_message = '<p>This {{child_pages_count}} macro can be called from wiki pages only</p>'
+
+    assert_equal expected_error_macro_message, textilizable('{{child_pages_count}}', object: issue)
   end
 end
