b3f7c4e6f81a479be94693c0999524d2dca6aaf0
otegami <otegami@clear-code.com>
2024-03-12 10:45:18 +0900

    fix typo
    - child_page_count
    + child_pages_count

--- lib/wiki_extensions_child_pages_count_macro.rb
+++ lib/wiki_extensions_child_pages_count_macro.rb
@@ -3,7 +3,7 @@ require 'redmine'
 module WikiExtensionsChildPagesCountMacro
   Redmine::WikiFormatting::Macros.register do
     desc "Displays the number of child pages.\n\n" +
-         "{{child_page_count}}\n"
+         "{{child_pages_count}}\n"
     macro :child_pages_count do |obj, args|
       raise t(:error_child_pages_count_macro) if [WikiContent, WikiContentVersion].none? { |wiki| obj.is_a?(wiki) }
 
