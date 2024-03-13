7f22c70b31611378cefdf1935b0dbb7c2e55f723
otegami <otegami@clear-code.com>
2024-03-13 09:43:07 +0900

    Remove duplicated words because the test class's name has already told the context

--- test/unit/wiki_extensions_child_pages_count_test.rb
+++ test/unit/wiki_extensions_child_pages_count_test.rb
@@ -12,31 +12,31 @@ class WikiExtensionsChildPagesCountTest < Redmine::HelperTest
     @project = nil
   end
 
-  def test_child_pages_count_in_wiki_content
+  def test_in_wiki_content
     @project = Project.find(1)
     wiki_page = WikiPage.find(2)
     assert_equal '<p>3</p>', textilizable('{{child_pages_count}}', object: wiki_page.content)
   end
 
-  def test_child_pages_count_in_wiki_content_version
+  def test_in_wiki_content_version
     @project = Project.find(1)
     wiki_page = WikiPage.find(2)
     assert_equal '<p>3</p>', textilizable('{{child_pages_count}}', object: wiki_page.content.versions.first)
   end
 
-  def test_child_pages_count_with_child_page
+  def test_with_child_page
     @project = Project.find(1)
     wiki_page = WikiPage.find(2)
     assert_equal '<p>1</p>', textilizable('{{child_pages_count(Child_1)}}', object: wiki_page.content)
   end
 
-  def test_child_pages_count_with_depth_option
+  def test_with_depth_option
     @project = Project.find(1)
     wiki_page = WikiPage.find(2)
     assert_equal '<p>2</p>', textilizable('{{child_pages_count(depth=1)}}', object: wiki_page.content)
   end
 
-  def test_child_pages_count_in_issue_page
+  def test_in_issue_page
     @project = Project.find(1)
     macro_error_message = <<-MESSAGE.gsub("\n", "")
 <p>
@@ -49,7 +49,7 @@ Error executing the <strong>child_pages_count</strong> macro
     assert_equal macro_error_message, textilizable('{{child_pages_count}}', object: Issue.first)
   end
 
-  def test_child_pages_count_in_wiki_without_permissions
+  def test_without_permissions
     @project = Project.find(1)
     another_project_wiki = WikiPage.find(3)
     macro_error_message = <<~MESSAGE.gsub("\n", "")
