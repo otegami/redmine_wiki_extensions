238e8ee19a236bdf6303c7f7158f3bbdf1bde022
takuya kodama <a.s.takuya1026@gmail.com>
2024-03-13 14:48:54 +0900

    Extend `child_pages_count` macro like `child_pages` macro (#5)
    I added the same API as `child_pages`macro in Redmine.
    I didn't add the only `parent` option because there is no use-case users want to create the link to the number of child pages.
    
    ref: `{{child_pages}}macro`
    
    https://github.com/redmine/redmine/blob/c3fe22476231adff5f6fef2e1692be8024dd0c44/lib/redmine/wiki_formatting/macros.rb#L193-L214

--- test/unit/wiki_extensions_child_pages_count_test.rb
+++ test/unit/wiki_extensions_child_pages_count_test.rb
@@ -1,22 +1,36 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class WikiExtensionsChildPagesCountTest < Redmine::HelperTest
-  include ApplicationHelper
   include ERB::Util
 
-  fixtures :projects, :wikis, :wiki_pages, :wiki_contents, :wiki_content_versions, :issues
+  fixtures :projects, :wikis, :wiki_pages, :wiki_contents, :wiki_content_versions, :issues,
+           :roles, :enabled_modules, :users
 
-  def test_child_pages_count_in_wiki_content
+  def setup
+    super
+    @project = Project.find(1)
+  end
+
+  def test_in_wiki_content
     wiki_page = WikiPage.find(2)
     assert_equal '<p>3</p>', textilizable('{{child_pages_count}}', object: wiki_page.content)
   end
 
-  def test_child_pages_count_in_wiki_content_version
+  def test_in_wiki_content_version
     wiki_page = WikiPage.find(2)
     assert_equal '<p>3</p>', textilizable('{{child_pages_count}}', object: wiki_page.content.versions.first)
   end
 
-  def test_child_pages_count_in_issue_page
+  def test_with_child_page
+    assert_equal '<p>1</p>', textilizable('{{child_pages_count(Child_1)}}')
+  end
+
+  def test_with_depth_option
+    wiki_page = WikiPage.find(2)
+    assert_equal '<p>2</p>', textilizable('{{child_pages_count(depth=1)}}', object: wiki_page.content)
+  end
+
+  def test_in_issue_page
     macro_error_message = <<-MESSAGE.gsub("\n", "")
 <p>
 <div class="flash error">
@@ -26,5 +40,17 @@ Error executing the <strong>child_pages_count</strong> macro
 </p>
     MESSAGE
     assert_equal macro_error_message, textilizable('{{child_pages_count}}', object: Issue.first)
+  end
+
+  def test_in_another_porject_wiki_without_permissions
+    another_project_wiki = Project.find(2).wiki.pages.first
+    macro_error_message = <<~MESSAGE.gsub("\n", "")
+<p>
+<div class="flash error">
+Error executing the <strong>child_pages_count</strong> macro (Page not found)
+</div>
+</p>
+    MESSAGE
+    assert_equal macro_error_message, textilizable('{{child_pages_count}}', object: another_project_wiki.content)
   end
 end
